627f0e093f11437283cefcb9e295644634eab8ab
Yashvardhan Jain <yashvardhan18@yahoo.com>
2020-02-22 21:11:54 +0530

    A1

---
+++ Assignment 1/Brokrage_Calculator.py
@@ -0,0 +1,8 @@
+def calculate_brokrage(buy_price, sell_price, quantity, type, exchange):
+    if type=="Equity Delivery":
+
+    elif type=="Equity Intraday":
+
+    elif type=="Equity Futures":
+
+    
